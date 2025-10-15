e99505e7a0fe09f775ef74478c54469f71974227
themillenniumfalcon <nishankpr2002@gmail.com>
2025-10-15 14:30:52 +0530

    added wireMesh

--- index.js
+++ index.js
@@ -18,8 +18,20 @@ camera.position.z = 2;
 const scene = new THREE.Scene();
 
 const geo = new THREE.IcosahedronGeometry(1.0, 2);
-const mat = new THREE.MeshNormalMaterial({ color: 0xccff });
+const mat = new THREE.MeshStandardMaterial({ color: 0xffffff, flatShading: true });
 const mesh = new THREE.Mesh(geo, mat);
 scene.add(mesh);
 
-renderer.render(scene, camera);
+const wireMat = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true });
+const wireMesh = new THREE.Mesh(geo, wireMat);
+scene.add(wireMesh);
+
+const hemLight = new THREE.HemisphereLight(0xffffff, 0x000000, 0.5);
+scene.add(hemLight);
+
+function animate(t = 0) {
+    requestAnimationFrame(animate);
+    // mesh.scale.setScalar(Math.cos(t * 0.001) + 1.0);
+    renderer.render(scene, camera);
+}
+animate();
