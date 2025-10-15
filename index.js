a075bf3c6832274c00ae9e6d05987fa436c2bb57
themillenniumfalcon <nishankpr2002@gmail.com>
2025-10-15 14:54:24 +0530

    orbital controls

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'jsm/controls/OrbitControls.js';
 
 const w = window.innerWidth;
 const h = window.innerHeight;
@@ -17,6 +18,10 @@ camera.position.z = 2;
 
 const scene = new THREE.Scene();
 
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.03;
+
 const geo = new THREE.IcosahedronGeometry(1.0, 2);
 const mat = new THREE.MeshStandardMaterial({ color: 0xffffff, flatShading: true });
 const mesh = new THREE.Mesh(geo, mat);
@@ -24,14 +29,17 @@ scene.add(mesh);
 
 const wireMat = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true });
 const wireMesh = new THREE.Mesh(geo, wireMat);
-scene.add(wireMesh);
+wireMesh.scale.setScalar(1.001);
+mesh.add(wireMesh);
 
-const hemLight = new THREE.HemisphereLight(0xffffff, 0x000000, 0.5);
+const hemLight = new THREE.HemisphereLight(0x0099ff, 0xaa5500, 0.5);
 scene.add(hemLight);
 
 function animate(t = 0) {
     requestAnimationFrame(animate);
     // mesh.scale.setScalar(Math.cos(t * 0.001) + 1.0);
+    // mesh.rotation.y = t * 0.0001;
     renderer.render(scene, camera);
+    controls.update();
 }
 animate();
